37535b6466bf8f159bb09940b8ad2458ff9290e0
Pakanon Pantisawat <pakanon.pk@gmail.com>
2021-04-25 20:36:31 +0700

    added GET /:id

--- app/routers/TaskRouter.scala
+++ app/routers/TaskRouter.scala
@@ -8,7 +8,7 @@ import play.api.routing.sird._
 
 class TaskRouter @Inject()(taskController: TaskController) extends SimpleRouter {
 	override def routes: Routes = {
-		case GET(p"/") => taskController.get
+		case GET(p"/") | GET(p"/$_") => taskController.get
 		case POST(p"/") => taskController.create
 	}
 }
