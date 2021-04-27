bc78ec4298ea5f1774393942a31aa181ecee5ebf
Pakanon Pantisawat <pakanon.pk@gmail.com>
2021-04-27 21:29:51 +0700

    edited parser to work correctly

--- app/helpers/TaskCreationSourceMapper.scala
+++ app/helpers/TaskCreationSourceMapper.scala
@@ -2,18 +2,37 @@ package helpers
 
 import jsonBodies.TaskCreationBody
 
+import scala.xml.Node
 import scala.xml.XML.loadString
 
 object TaskCreationSourceMapper {
+
+	private def getClassName(node: Node) = (node \ "@class").toString()
+
+	private def getEnclosure(code: String, modifier: String): String =
+		"::elab:endcode\n"
+			.concat(s"::elab:begincode $modifier=True\n")
+			.concat(code)
+			.concat("\n::elab:endcode\n")
+			.concat("::elab:begincode\n")
+
+	private def nodeToText(node: Node): String = node match {
+		case <br/> => "\n"
+		case elem if elem.label == "span" && getClassName(elem) == "sourcespan" =>
+			s"{{ ${elem.child.map(nodeToText).mkString("")} }}"
+		case elem if elem.label == "span" && getClassName(elem) == "hidespan" =>
+			getEnclosure(elem.child.map(nodeToText).mkString(""), "hidden")
+		case elem if elem.label == "span" && getClassName(elem) == "boxspan" =>
+			getEnclosure(elem.child.map(nodeToText).mkString(""), "blank")
+		case elem => elem.text
+	}
+
 	private def parse(source: String): String = {
 		val replacedSource = source.replaceAll("&nbsp;", " ")
 
-		loadString(replacedSource).child
-			.map {
-				case <br/> => "\n"
-				case elem if elem.child.nonEmpty => s" {{ ${elem.child.head} }} "
-				case elem => elem.toString()
-			}
+		loadString(replacedSource)
+			.child
+			.flatMap(_.child.map(nodeToText).prepended("::elab:begincode\n").concat("::elab:endcode\n"))
 			.mkString("")
 	}
 
